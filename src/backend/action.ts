4041f5be61030b5eff1092d7132e15951e64419d
TurboHsu <47477044+TurboHsu@users.noreply.github.com>
2024-01-24 16:54:35 +0800

    🎨 fine code

--- src/backend/action.ts
+++ src/backend/action.ts
@@ -3,32 +3,32 @@ import sleep from "../libs/utils";
 
 // Read image from the page
 async function readImage(tabId: number, xpath: string): Promise<string> {
-    // Send message to content script
-    return browser.tabs
-        .sendMessage(tabId, {
-            action: "read-image",
-            xpath: xpath,
-        })
-        .then(async (response: any) => {
-            console.log("From content-script:", response);
-            if (response.status === "ok") {
-                // Maybe the image is not loaded yet
-                if (response.data === "data:,") {
-                    console.log("Image not loaded yet, retrying...");
-                    // Wait for 1 second
-                    await sleep(1000);
-                    // Try again
-                    return await readImage(tabId, xpath);
-                } else {
-                    return response.data;
-                }
-            }
-        });
+	// Send message to content script
+	return browser.tabs
+		.sendMessage(tabId, {
+			action: "read-image",
+			xpath: xpath,
+		})
+		.then(async (response: any) => {
+			console.log("From content-script:", response);
+			if (response.status === "ok") {
+				// Maybe the image is not loaded yet
+				if (response.data === "data:,") {
+					console.log("Image not loaded yet, retrying...");
+					// Wait for 1 second
+					await sleep(1000);
+					// Try again
+					return await readImage(tabId, xpath);
+				} else {
+					return response.data;
+				}
+			}
+		});
 }
 
 // Fills input box
 async function fillInputBox(tabId: number, xpath: string, data: string) {
-	await browser.tabs
+	return await browser.tabs
 		.sendMessage(tabId, {
 			action: "fill-input",
 			xpath: xpath,
@@ -39,7 +39,6 @@ async function fillInputBox(tabId: number, xpath: string, data: string) {
 				console.log("Filled input box");
 			}
 		});
-	throw new Error("Failed to fill input box");
 }
 
 export { readImage, fillInputBox };
