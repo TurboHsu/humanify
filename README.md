55b38b5453ad8ccc4e9d6937bac69eb82d3be3fc
TurboHsu <47477044+TurboHsu@users.noreply.github.com>
2025-02-16 22:31:49 +0800

    📝 update README.md

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
-# Humanify | WIP
+# Humanify
 
-## What is it?
+A browser extension to help you fill in image captchas.
 
-Cross-platform browser extension to help you fill in image captchas.
+## How to use
 
-## How to use it?
+Open extension options page, create a new rule.
 
-**Turns out that you just can't!**
+![New rule](./assets/pic/new_rule.png)
 
-I'm still making this shit.
+Write a `regex expression` to match the target URL you want to apply the rule to.
+
+Open `Devtool`, crank up `element picker`, select the captcha image and copy `XPath`.
+
+Same for the input field.
+
+When everything is ready, save the rule. Then just reload the target page, the extension will automatically fill in the captcha for you, just like this:
+
+![yay](./assets/pic/yay.png)
+
+## How to build
+
+Clone the repo, run `pnpm i` to install dependencies.
+
+Build with:
+
+```bash
+TARGET=firefox pnpm build
+```
+
+If you want to build for Chrome, just modify environment variable `TARGET` to `chrome`.
+
+Then just load the extension in your browser.
+
+## Credits
+
+[ddddocr](https://github.com/sml2h3/ddddocr) for captcha image recognition.
--- src/backend/init.ts
+++ src/backend/init.ts
@@ -5,22 +5,23 @@ async function init() {
 	console.log(await browser.storage.sync.get("enabledFilter"));
 	// Checks if the database is empty and if so, creates empty tables
 	// and inserts some default data.
-	// if (browser.storage.sync.get("enabledFilter") === undefined) {
-	// browser.storage.sync.set({ enabledFilter: [] });
-	browser.storage.sync.set({
-		enabledFilter: ["https://authserver.nuist.edu.cn/*"],
-	});
-	// }
+	if (browser.storage.sync.get("enabledFilter") === undefined) {
+		// browser.storage.sync.set({ enabledFilter: [] });
+		browser.storage.sync.set({
+			enabledFilter: ["https://authserver.nuist.edu.cn/*"],
+		});
+	}
 
-	// if (browser.storage.sync.get("filterActions") === undefined) {
-	browser.storage.sync.set({
-		filterActions: {
-			"https://authserver.nuist.edu.cn/*": <Action[]>[
-				{ image: '//*[@id="captchaImg"]', input: '//*[@id="captcha"]' },
-			],
-		},
-	});
-	console.log("Finished Init")
+	if (browser.storage.sync.get("filterActions") === undefined) {
+		browser.storage.sync.set({
+			filterActions: {
+				"https://authserver.nuist.edu.cn/*": <Action[]>[
+					{ image: '//*[@id="captchaImg"]', input: '//*[@id="captcha"]' },
+				],
+			},
+		});
+	}
+	console.log("Finished Init");
 	// }
 }
 
